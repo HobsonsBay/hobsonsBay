d463b46dc739334c96d33f0d0c16d0f36a1f6cf9
Alex Reid <alex@codeforaustralia.org>
2020-07-22 14:42:36 +1000

    added initial stlysheet work

---
+++ mobile/src/utils/styles.js
@@ -0,0 +1,45 @@
+export const styles = {
+	type : {
+		headings : {
+			small : {
+
+			},
+			medium : {
+
+			},
+			large : {
+
+			}
+		},
+		paras : {
+			default: {
+
+			},
+			intro: {
+
+			}
+		},
+		bold : {
+
+		},
+		italic : {
+
+		},
+		list : {
+
+		}
+	},
+	colours : {
+		black : "#000",
+		white : "#fff",
+		blue : "#00f",
+		green_fogo: '#9ACA3C',
+		green_rubbish: '#536130',
+		yellow_recycle: '#FFDC00',
+		purple_glass: '#A032A0',
+		grey : '#D8D8D8'
+	},
+	spacing : {
+
+	}
+}
