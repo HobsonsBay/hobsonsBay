ba4ae7ef4e97d39f6d530126bdb75844b9b6c0af
alex-reid <alex@alex-reid.com.au>
2021-05-31 13:30:51 +1000

    file updates

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,3 +1,10 @@
+ /*
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import 'react-native-gesture-handler';
 import React from 'react';
 import { createDrawerNavigator, 
